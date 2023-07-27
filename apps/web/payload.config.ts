900ee46e72a567239a67e2aa1d9a74d0ce83c7a0
MP <markpaololibunao@gmail.com>
2023-07-27 17:11:08 +0800

    shit

--- apps/web/payload.config.ts
+++ apps/web/payload.config.ts
@@ -3,8 +3,6 @@ import { buildConfig } from 'payload/config'
 
 import FormSubmission from './src/cms/collections/FormSubmission'
 
-//import { getDirname } from './src/shared/esm.ts'
-
 // By default, Payload will boot up normally
 // and you will be provided with a base `User` collection.
 // But, here is where you define how you'd like Payload to work!
@@ -12,16 +10,10 @@ export default buildConfig({
 	serverURL: 'http://localhost:3000',
 	collections: [FormSubmission],
 	typescript: {
-		outputFile: path.resolve(
-			//getDirname(import.meta.url),
-			'./src/cms/payload-types.ts',
-		),
+		outputFile: path.resolve('./src/cms/payload-types.ts'),
 	},
 	graphQL: {
-		schemaOutputFile: path.resolve(
-			//getDirname(import.meta.url),
-			'./src/cms/generated-schema.graphql',
-		),
+		schemaOutputFile: path.resolve('./src/cms/generated-schema.graphql'),
 	},
 	telemetry: false,
 })
