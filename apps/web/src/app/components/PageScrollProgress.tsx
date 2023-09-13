bc6a9483c99a639ccc86acc1353c1d02b67b76ed
MP <markpaololibunao@gmail.com>
2023-09-13 10:56:07 +0800

    Discovers inset tw classes which is a shorthand for top/bottom/left/right

--- apps/web/src/app/components/PageScrollProgress.tsx
+++ apps/web/src/app/components/PageScrollProgress.tsx
@@ -8,7 +8,7 @@ export const PageScrollProgress = (): JSX.Element => {
 		<LazyMotionDomAnimation>
 			<m.div
 				style={{ scaleX: scrollYProgress }}
-				className='fixed top-0 left-0 right-0 h-2 bg-red origin-left z-40'
+				className='fixed top-0 inset-x-0 h-2 bg-red origin-left z-40'
 			/>
 		</LazyMotionDomAnimation>
 	)
